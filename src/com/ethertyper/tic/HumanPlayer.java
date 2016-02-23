e307bc217eb06a8d82077a0fe4be0f11121786a0
ELI JOSEPH BRADLEY <EtherTyper@users.noreply.github.com>
2016-02-23 12:36:47 -0600

    Reorganize HumanPlayer class

--- src/com/ethertyper/tic/HumanPlayer.java
+++ src/com/ethertyper/tic/HumanPlayer.java
@@ -28,45 +28,52 @@ public class HumanPlayer extends Player {
 	private int[] tmpFreeRow = new int[9], tmpFreeCol = new int[9];
 
 	public synchronized int chooseNextMove(char[][] grid, char whichPlayer) {
-        String visual="";
-        // 1: +-+-+-+
-        // 2: | | | |
-        // 3: +-+-+-+
-        // 4: | | | |
-        // 5: +-+-+-+
-        for (int i=0; i<grid.length; i++){
-            visual+=oddLine(grid[i]);
-            visual+=evenLine(grid[i]);
-            if (i==grid[i].length-1)
-                visual+=oddLine(grid[i]);
-        }
-        int choice=0;
-        do {
-            System.out.println(visual+"\nPlant "+whichPlayer+" in... ");
-            Scanner scan=new Scanner(System.in);
-            choice=(scan.nextInt()%3)*3+(scan.nextInt()%3);
-        } while (grid[choice/3][choice%3]!=' ');
-        return choice;
-		// throw new InternalError("Unexpected grid");
+		showGrid(grid);
+
+		int choice=0;
+		do {
+			System.out.println(visual+"\nPlant "+whichPlayer+" in... ");
+			Scanner scan=new Scanner(System.in);
+			choice=(scan.nextInt()%3)*3+(scan.nextInt()%3);
+		} while (grid[choice/3][choice%3]!=' ');
+
+		return choice;
+	//	throw new InternalError("Unexpected grid");
+	}
+
+	public synchronized String showGrid(char[][] grid){
+		String visual="";
+		
+		// 1: +-+-+-+
+		// 2: | | | |
+		// 3: +-+-+-+
+		// 4: | | | |
+		// 5: +-+-+-+
+		
+		for (int i=0; i<grid.length; i++){
+			visual+=oddLine(grid[i]);
+			visual+=evenLine(grid[i]);
+			if (i==grid[i].length-1)
+				visual+=oddLine(grid[i]);
+		}
+	}
+	public synchronized String oddLine(char[] grid){
+		String visual="";
+		for (int j=0; j<grid.length; j++){
+			visual+="+-";
+			if (j==grid.length-1)
+				visual+="+\n";
+		}
+		return visual;
 	}
     
-    public synchronized String oddLine(char[] grid){
-        String visual="";
-        for (int j=0; j<grid.length; j++){
-            visual+="+-";
-            if (j==grid.length-1)
-                visual+="+\n";
-        }
-        return visual;
-    }
-    
-    public synchronized String evenLine(char[] grid){
-        String visual="";
-        for (int j=0; j<grid.length; j++){
-            visual+="|"+grid[j];
-            if (j==grid.length-1)
-                visual+="|\n";
-        }
-        return visual;
-    }
+	public synchronized String evenLine(char[] grid){
+		String visual="";
+		for (int j=0; j<grid.length; j++){
+			visual+="|"+grid[j];
+			if (j==grid.length-1)
+				visual+="|\n";
+		}
+		return visual;
+	}
 }
